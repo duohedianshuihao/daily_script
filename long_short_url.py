23d38eb28288230f388e655a13a9b6d0ab961ea1
duohedianshuihao <cuizhanyuan1111@gmail.com>
2018-05-06 00:02:02 -0400

    add base 62 convert

---
+++ long_short_url.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import string
+
+CHAR_D = {i: item for i, item in enumerate(string.ascii_letters + string.digits)}
+
+
+def convert(number):
+    if not isinstance(number, int):
+        return
+    base62_n = []
+    while number > 0:
+        number, reminder = divmod(number, 62)
+        base62_n.append(reminder)
+    print base62_n[::-1]
+    s = ''.join(map(lambda x: CHAR_D[x], base62_n[::-1]))
+    print s
+
+
+if __name__ == '__main__':
+    # 发号策略，对来的号码进行62进制编码，转换成短址
+    convert(1234)
