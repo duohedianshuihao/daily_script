58a8c327cc33d6665e7eb4acd10c44af099a2282
duohedianshuihao <cuizhanyuan1111@gmail.com>
2018-05-05 21:16:57 -0400

    add daily script

---
+++ random_string.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+import random
+import string
+import getopt
+import sys
+
+
+def get_random_string(length=15):
+    print ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(int(length)))
+
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'l:')
+    except getopt.GetoptError:
+        sys.stdout.write("NOTE: ignoring args rather than -l, using default length 15\n")
+        get_random_string()
+    else:
+        opts_d = {t[0]: t[1] for t in opts}
+        get_random_string(opts_d.get('-l'))
---
+++ who_is_here.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+import requests
+import time
+import os
+
+URL = os.environ.get('HOME_LOGIN_URL', None)
+USER = os.environ.get('HOME_USER', None)
+PASSWORD = os.environ.get('HOME_PASSWORD', None)
+USER_LIST_URL = os.environ.get('HOME_USER_LIST_URL', None)
+
+
+class CouldNotGetEnviron(Exception):
+    pass
+
+
+class TimedOut(Exception):
+    pass
+
+
+def main():
+    if not all([URL, USER, PASSWORD, USER_LIST_URL]):
+        raise CouldNotGetEnviron
+    s = requests.Session()
+    data = {
+        'user': USER,
+        'pws': PASSWORD
+    }
+    try:
+        r = s.post(URL, data=data)
+    except requests.exceptions.ConnectionError:
+        raise TimedOut
+    if r.status_code != 200:
+        return
+    params = {'_': int(time.time() * 1000)}
+    users = s.get(USER_LIST_URL, params=params)
+    for user in eval(users.content):
+        hostName = user.get('hostName', None).lower()
+        if not hostName or hostName == 'unknown':
+            continue
+        print hostName
+
+
+if __name__ == '__main__':
+    main()
